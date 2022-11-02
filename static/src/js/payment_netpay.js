cf53b9e5db46faf94d4dfb2ecd1c846bdcf02b39
Geordie-Castaneda <jordyorantes9@gmail.com>
2022-11-02 15:14:01 -0600

    Eliminando logs

--- static/src/js/payment_netpay.js
+++ static/src/js/payment_netpay.js
@@ -4,13 +4,12 @@ odoo.define('pos_netpay_connector.payment', function(require) {
      var PaymentInterface = require('point_of_sale.PaymentInterface');
      const { Gui } = require('point_of_sale.Gui');
      const rpc = require('web.rpc');
- 
+
      var _t = core._t;
- 
+
      var PaymentNetpay = PaymentInterface.extend({
- 
+
             send_payment_request: function(cid) {
-                console.log('Bienvenido a la función send_payment_request')
                 this._super.apply(this, arguments);
                 var line = this.pos.get_order().selected_paymentline;
                 var order = this.pos.get_order();
@@ -27,18 +26,16 @@ odoo.define('pos_netpay_connector.payment', function(require) {
                  //});
                 return this._netpay_pay()
              },
-         
+
             // private methods
             _reset_state: function () {
-                console.log('Funcion _reset_state')
                 this.was_cancelled = false;
                 this.last_diagnosis_service_id = false;
                 this.remaining_polls = 4;
                 clearTimeout(this.polling);
-            },         
- 
+            },
+
             _call_netpay: function (data, operation='sale') {
-                console.log('Función _call_netpay')
                 return rpc.query({
                     model: 'pos.payment.method',
                     method: 'proxy_netpay_request',
@@ -55,9 +52,7 @@ odoo.define('pos_netpay_connector.payment', function(require) {
                 var order = this.pos.get_order();
                 var config = this.pos.config;
                 var line = order.selected_paymentline;
-                
-                console.log('Prestad atención aquí ')
-                console.log(config);
+
                 var serial_number = false;
                 if(config.serial_number){
                     serial_number = config.serial_number;
@@ -70,12 +65,12 @@ odoo.define('pos_netpay_connector.payment', function(require) {
                 if(config.id){
                     config_id = config.id;
                 }
-                
+
                 var access_token = false;
                 if(config.access_token){
                     access_token = config.access_token;
                 }
-                
+
                 var serial_number = false;
                 if (config.serial_number){
                     serial_number = config.serial_number;
@@ -91,16 +86,15 @@ odoo.define('pos_netpay_connector.payment', function(require) {
                         'type': "sale",
                         'config_id': config_id,
                         'serial_number': serial_number,
-                    }               
+                    }
                 }
 
                 return data;
             },
-        
+
             _netpay_pay: function () {
                 var self = this;
                 var order = this.pos.get_order();
-                console.log('Función Netpay_pay')
                 if (order.selected_paymentline.amount < 0) {
                     this._show_error(_t('Cannot process transactions with negative amount.'));
                     return Promise.resolve();
@@ -116,7 +110,7 @@ odoo.define('pos_netpay_connector.payment', function(require) {
                     return self._netpay_handle_response(data);
                 });
             },
-            
+
             _show_error: function (msg, title) {
                 if (!title) {
                     title =  _t('NETPAY Error');
@@ -127,18 +121,16 @@ odoo.define('pos_netpay_connector.payment', function(require) {
                 });
             },
 
-         
+
             _netpay_handle_response: function (response) {
                 var line = this.pos.get_order().selected_paymentline;
 
-                console.log('Función netpay_handle_response')
                 if (response.error && response.error.status_code != 200) {
-                    console.log(response.error.message)
                     this._show_error(_t(response.error.message.toString()));
                     line.set_payment_status('force_done');
                     return Promise.resolve();
                 }
-                
+
                 response = response.SaleToPOIRequest;
                 if (response && response.EventNotification && response.EventNotification.EventToNotify == 'Reject') {
                     console.error('error from Adyen', response);
@@ -176,12 +168,11 @@ odoo.define('pos_netpay_connector.payment', function(require) {
 
                     return res;
                 }
-            },         
+            },
 
            _terminal_pay_data: function() {
                   var order = this.pos.get_order();
                   var line = order.selected_paymentline;
-                  console.log('Función terminal_pay_data')
                   var data = {
                         'Name': order.name,
                         'OrderID': order.uid,
@@ -192,7 +183,7 @@ odoo.define('pos_netpay_connector.payment', function(require) {
                   };
                  return data;
              },
-         
+
          _handle_odoo_connection_failure: function (data) {
              // handle timeout
              var line = this.pending_netpay_line();
@@ -203,14 +194,14 @@ odoo.define('pos_netpay_connector.payment', function(require) {
 
             return Promise.reject(data); // prevent subsequent onFullFilled's from being called
          },
-         
+
          pending_adyen_line() {
              return this.pos.get_order().paymentlines.find(
              paymentLine => paymentLine.payment_method.use_payment_terminal === 'netpay' && (!paymentLine.is_done()));
          },
-         
+
          clear_serial_id: function(id){
-             
+
             return rpc.query({
                model: 'pos.order',
                method: 'delete_values',
@@ -219,19 +210,13 @@ odoo.define('pos_netpay_connector.payment', function(require) {
                timeout: 10000,
                shadow: true,
             }).then(function (ok){
-                console.log('Lo que sea')
-                console.log('')
-                console.log('')
-                console.log('')
+                console.log('Un 1')
             })
          },
-         
+
          _poll_for_response: function (resolve, reject) {
              var self = this;
              var order = self.pos.get_order();
-             console.log('Resolve ')
-             console.log(resolve);
-             console.log(reject)
              if (this.was_cancelled) {
                 resolve(false);
                 return Promise.resolve();
@@ -256,37 +241,16 @@ odoo.define('pos_netpay_connector.payment', function(require) {
                 // this promise don't resolve -- that is, it doesn't go to the 'then' clause.
                 return Promise.reject(data);
             }).then(function (status) {
-                console.log('Intentando entrar al THEN ::::::')
-                console.log(status)
                 var notification = status.latest_response;
                 var order = self.pos.get_order();
                 var line = self.pending_adyen_line();
-                console.log(notification);
-                console.log(self.most_recent_service_id);
-                console.log('');
-                console.log('ahora que es order?');
-                console.log(order);
-                console.log(line)
-                console.log('');
-//                 order.set_netpay_orderId(notification);
-                console.log('')
-                console.log('')
-                console.log(':D')
-                console.log(notification.responseCode)
-                if(notification.responseCode != '00'){
-                    console.log('Si es diferente :D')
-                    console.log('')
-                    console.log('')
-                }
                 var additional_response = new URLSearchParams(notification.message);
-//                 si mi linea es retry que entre a un if creado para 
-                
-                console.log('Que estado es line')
+//                 si mi linea es retry que entre a un if creado para
+
                 if (notification && notification.orderId ) {
 //                     var response = notification.SaleToPOIResponse.PaymentResponse.Response;
                     var additional_response = new URLSearchParams(notification.message);
-//                     console.log('Que es response :/')
-//                     console.log(response)
+
                     if (notification.responseCode == '00' && notification.folioNumber == order.uid) {
                         var config = self.pos.config;
 //                         Si el estado es retry meter el codigo de order.set_netpay_orderId(notification); resolve(true);
@@ -294,8 +258,8 @@ odoo.define('pos_netpay_connector.payment', function(require) {
                         order.set_netpay_orderId(notification);
                         resolve(true);
                     } else if(notification.responseCode != '00' && notification.folioNumber == order.uid){
-//                      Mostrar error y toda la cosas 
-                        console.log('Que estoy recibiendo?')
+//                      Mostrar error y toda la cosas
+
                         var message = additional_response.get('message');
                         self._show_error(_.str.sprintf(_t('Message from Adyen: %s'), message));
                         // this means the transaction was cancelled by pressing the cancel button on the device
@@ -308,29 +272,22 @@ odoo.define('pos_netpay_connector.payment', function(require) {
 
                         // this means the transaction was cancelled by pressing the cancel button on the device
                         resolve(false);
-                        
+
                     }
                 } else if(notification && notification.responseCode != "00" ){
-//                     Mostrar error y toda la cosas 
-                    console.log('Mirar aquí iiii Hay algo diferente ')
-                    console.log(notification)
+//                     Mostrar error y toda la cosas
 //                     var message = additional_response.get('message');
                     self._show_error(_.str.sprintf(_t(notification.message)));
                     // this means the transaction was cancelled by pressing the cancel button on the device
-                    console.log('************')
-                    console.log(line)
-                    console.log('-')
-                    console.log('-')
-                    console.log('-')
-//                     Crear un RPC que que busque el pos.payment.method que lleve el numero de serial igualito al controller "serial_number", lo deje en blanco 
+//                     Crear un RPC que que busque el pos.payment.method que lleve el numero de serial igualito al controller "serial_number", lo deje en blanco
                     self.clear_serial_id(line.payment_method.id)
                     resolve(false);
 //                     return Promise.resolve();
 //                     line.set_payment_status('retry');
-                    
+
 //                     reject();
                 }
-                
+
                 else {
                     line.set_payment_status('waitingCard')
                 }
@@ -341,8 +298,8 @@ odoo.define('pos_netpay_connector.payment', function(require) {
             var config = this.pos.config;
             return _.str.sprintf('%s (ID: %s)', config.display_name, config.id);
         },
-         
-         
+
+
      });
     return PaymentNetpay;
 });
